53a8efeb840629077328bdf28983fb302962dd70
rhynodegreat <rhynodegreat@gmail.com>
2017-01-13 17:01:30 -0600

    Add Window

---
+++ UnnamedEngine/Core/Window.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+using CSGL.GLFW;
+
+namespace UnnamedEngine.Core {
+    public class Window : IDisposable {
+        WindowPtr window;
+        bool disposed;
+
+        public Window(Engine engine, int width, int height, string title) {
+            GLFW.WindowHint(WindowHint.ClientAPI, (int)ClientAPI.NoAPI);
+            window = GLFW.CreateWindow(width, height, title, MonitorPtr.Null, WindowPtr.Null);
+        }
+
+        public void Dispose() {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        void Dispose(bool disposing) {
+            if (disposed) return;
+
+            GLFW.DestroyWindow(window);
+        }
+
+        ~Window() {
+            Dispose(false);
+        }
+    }
+}
