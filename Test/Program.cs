d3454b370c60d7359867cf2c53301d5fe1781ee2
rhynodegreat <rhynodegreat@gmail.com>
2017-01-13 16:50:06 -0600

    Update project

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
+using CSGL.GLFW;
+using CSGL.Vulkan;
+
+using UnnamedEngine.Core;
 
 namespace Test {
     class Program {
@@ -10,8 +12,31 @@ namespace Test {
             new Program().Run();
         }
 
-        void Run() {
+        string[] layers = {
+            "VK_LAYER_LUNARG_standard_validation",
+            //"VK_LAYER_LUNARG_api_dump"
+        };
 
+        void Run() {
+            GLFW.Init();
+
+            Instance instance = CreateInstance();
+            Device device = null;
+
+            Renderer renderer = new Renderer(instance, device);
+            Engine engine = new Engine(renderer);
+
+            GLFW.Terminate();
+        }
+
+        Instance CreateInstance() {
+            ApplicationInfo appInfo = new ApplicationInfo(new VkVersion(1, 0, 0), new VkVersion(0, 0, 0), new VkVersion(0, 1, 0), "Test", "Unnamed Engine");
+            InstanceCreateInfo info = new InstanceCreateInfo(appInfo, GLFW_VK.GetRequiredInstanceExceptions(), layers);
+            return new Instance(info);
+        }
+
+        Device CreateDevice(Instance instance) {
+            return null;
         }
     }
 }
