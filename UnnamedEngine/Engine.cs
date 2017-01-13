436fb55b8bbfe8af94dd3555531c3a3a0bace36e
rhynodegreat <rhynodegreat@gmail.com>
2017-01-13 16:34:13 -0600

    Add Engine

---
+++ UnnamedEngine/Engine.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UnnamedEngine {
+    public class Engine : IDisposable {
+        bool disposed;
+
+        public Engine() {
+
+        }
+
+        public void Dispose() {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        void Dispose(bool disposing) {
+            if (disposed) return;
+
+            disposed = true;
+        }
+
+        ~Engine() {
+            Dispose(false);
+        }
+    }
+}
