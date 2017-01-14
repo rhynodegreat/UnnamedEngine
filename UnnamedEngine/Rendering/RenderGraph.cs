5f9479dd4f71b9c92cc25e9e332271a483c1efd8
rhynodegreat <rhynodegreat@gmail.com>
2017-01-14 17:59:54 -0600

    Add constructor

--- UnnamedEngine/Rendering/RenderGraph.cs
+++ UnnamedEngine/Rendering/RenderGraph.cs
@@ -1,9 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 
+using UnnamedEngine.Core;
+
 namespace UnnamedEngine.Rendering {
     public class RenderGraph {
         List<RenderNode> start;
+
+        Renderer renderer;
+
+        public RenderGraph(Renderer renderer) {
+            if (renderer == null) throw new ArgumentNullException(nameof(renderer));
+            this.renderer = renderer;
+            start = new List<RenderNode>();
+        }
 
         public void AddStart(RenderNode node) {
             if (node == null) throw new ArgumentNullException(nameof(node));
