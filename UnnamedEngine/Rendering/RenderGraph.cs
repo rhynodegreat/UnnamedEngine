4581ea4675a6f9b3e49cdae3f76cc68cc9a3c009
rhynodegreat <rhynodegreat@gmail.com>
2017-01-14 17:40:27 -0600

    Add RenderGraph and RenderNode

---
+++ UnnamedEngine/Rendering/RenderGraph.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UnnamedEngine.Rendering {
+    public class RenderGraph {
+        List<RenderNode> start;
+
+        public void AddStart(RenderNode node) {
+            if (node == null) throw new ArgumentNullException(nameof(node));
+            if (start.Contains(node)) return;
+            start.Add(node);
+        }
+
+        public void RemoveStart(RenderNode node) {
+            if (node == null) throw new ArgumentNullException(nameof(node));
+            if (!start.Contains(node)) return;
+            start.Remove(node);
+        }
+    }
+
+    public class RenderGraphException : Exception {
+        public RenderGraphException(string message) : base(message) { }
+        public RenderGraphException(string format, params object[] args) : base(string.Format(format, args)) { }
+    }
+}
---
+++ UnnamedEngine/Rendering/RenderNode.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+using CSGL.Vulkan;
+
+namespace UnnamedEngine.Rendering {
+    public abstract class RenderNode {
+        public IEnumerable<RenderNode> Input { get; protected set; }
+        public IEnumerable<RenderNode> Output { get; protected set; }
+        public Semaphore SignalSemaphore { get; protected set; }
+        public Semaphore WaitSemaphore { get; protected set; }
+
+        protected abstract void AddInput(RenderNode node);
+        protected abstract void RemoveInput(RenderNode node);
+
+        protected abstract void AddOutput(RenderNode node);
+        protected abstract void RemoveOutput(RenderNode node);
+
+        public abstract CommandBuffer GetCommandbuffer();
+
+        public static void Compose(RenderNode a, RenderNode b) {
+            a.AddOutput(b);
+            b.AddInput(a);
+        }
+
+        public static void Uncompose(RenderNode a, RenderNode b) {
+            a.RemoveOutput(b);
+            b.RemoveInput(a);
+        }
+    }
+}
