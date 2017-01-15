486f6cdc6cce81f8bdeeea179c59e7e5c9f7b5c2
rhynodegreat <rhynodegreat@gmail.com>
2017-01-14 23:05:54 -0600

    Change interface

--- UnnamedEngine/Rendering/RenderNode.cs
+++ UnnamedEngine/Rendering/RenderNode.cs
@@ -6,27 +6,12 @@ using CSGL.Vulkan;
 namespace UnnamedEngine.Rendering {
     public abstract class RenderNode {
         public IEnumerable<RenderNode> Input { get; protected set; }
-        public IEnumerable<RenderNode> Output { get; protected set; }
         public Semaphore SignalSemaphore { get; protected set; }
-        public Semaphore WaitSemaphore { get; protected set; }
 
-        protected abstract void AddInput(RenderNode node);
-        protected abstract void RemoveInput(RenderNode node);
+        public abstract void AddInput(RenderNode node);
+        public abstract void RemoveInput(RenderNode node);
 
-        protected abstract void AddOutput(RenderNode node);
-        protected abstract void RemoveOutput(RenderNode node);
-
-        public abstract CommandBuffer GetCommandBuffer();
-
-        public static void Compose(RenderNode a, RenderNode b) {
-            a.AddOutput(b);
-            b.AddInput(a);
-        }
-
-        public static void Uncompose(RenderNode a, RenderNode b) {
-            a.RemoveOutput(b);
-            b.RemoveInput(a);
-        }
+        public abstract SubmitInfo GetSubmitInfo();
     }
 
     public class RenderNodeException : Exception {
