5f81eb5281b93c1d70dd34db7da5dfaa22f8430d
rhynodegreat <rhynodegreat@gmail.com>
2017-01-21 14:32:36 -0600

    Move internal classes to their own files

---
+++ UnnamedEngine/Utilities/VkAlloc/Heap.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+
+using CSGL.Vulkan;
+
+namespace UnnamedEngine.Utilities {
+    public partial class VkAllocator {
+        public class Heap : IDisposable {
+            List<Page> pages;
+            object locker;
+            Device device;
+            List<VkMemoryPropertyFlags> heapFlags;
+            HashSet<int> typeIndices;
+            int numTypes;
+            ulong pageSize;
+            Dictionary<DeviceMemory, Page> pageMap;
+
+            public Heap(Device device, int heapIndex, VkPhysicalDeviceMemoryProperties props, ulong pageSize, Dictionary<DeviceMemory, Page> pageMap) {
+                this.device = device;
+                pages = new List<Page>();
+                locker = new object();
+                numTypes = (int)props.memoryTypeCount;
+                this.pageSize = pageSize;
+                this.pageMap = pageMap;
+
+                heapFlags = new List<VkMemoryPropertyFlags>();
+                typeIndices = new HashSet<int>();
+
+                for (int i = 0; i < props.memoryTypeCount; i++) {
+                    var type = props.GetMemoryTypes(i);
+                    if (type.heapIndex == heapIndex) {
+                        typeIndices.Add(i);
+                        heapFlags.Add(type.propertyFlags);
+                    }
+                }
+            }
+
+            public bool Match(uint memoryBits, VkMemoryPropertyFlags flags, out int typeIndex) {
+                bool typeMatch = false;
+                typeIndex = -1;
+
+                for (int i = 0; i < numTypes; i++) {
+                    if ((memoryBits & (1 << i)) != 0 && typeIndices.Contains(i)) {
+                        typeMatch = true;
+                        typeIndex = i;
+                        break;
+                    }
+                }
+
+                bool flagMatch = false;
+
+                for (int i = 0; i < heapFlags.Count; i++) {
+                    if ((heapFlags[i] & flags) == flags) {
+                        flagMatch = true;
+                        break;
+                    }
+                }
+
+                return typeMatch && flagMatch;
+            }
+
+            public VkaAllocation Alloc(VkMemoryRequirements requirements, int typeIndex) {
+                lock (locker) {
+                    for (int i = 0; i < pages.Count; i++) {
+                        VkaAllocation result = pages[i].AttemptAlloc(requirements);
+                        if (result.memory != null) {
+                            return result;
+                        }
+                    }
+
+                    ulong size = Math.Max(requirements.size, pageSize);
+                    Page newPage = new Page(device, size, typeIndex, pageMap);
+                    pages.Add(newPage);
+
+                    return newPage.AttemptAlloc(requirements);
+                }
+            }
+
+            public void Dispose() {
+                for (int i = 0; i < pages.Count; i++) {
+                    pages[i].Dispose();
+                }
+            }
+        }
+    }
+}
---
+++ UnnamedEngine/Utilities/VkAlloc/Node.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnnamedEngine.Utilities {
+    public partial class VkAllocator {
+        class Node {
+            public Node next;
+            public ulong offset;
+            public ulong size;
+            public bool free;
+
+            public Node(ulong offset, ulong size) {
+                this.offset = offset;
+                this.size = size;
+                free = true;
+            }
+
+            public void Split(ulong start, ulong size) {
+                //split a node and mark the correct one as not free
+                //this node can potentially be split into three if start and size defines a space in the middle of the node
+
+                if (start == offset && this.size == size) {
+                    //entire node was taken, so mark this a not free
+                    free = false;
+                } else if (start > offset) {
+                    //some space was left in the beginning, so use this node for that and mark as new one a not free
+                    ulong startSpace = start - offset;
+                    this.size = startSpace;
+
+                    Node middle = new Node(start, size);
+                    middle.next = next;
+                    next = middle;
+
+                    //new node might need to be split
+                    middle.Split(start, size);
+                } else {
+                    //only some space left at the back
+                    free = false;
+                    ulong endOffset = start + size;
+                    ulong endSpace = (offset + this.size) - endOffset;
+
+                    Node end = new Node(endOffset, endSpace);
+                    end.next = next;
+                    next = end;
+                }
+            }
+
+            public void Merge() {
+                if (free) {
+                    Node next = this.next;
+                    while (next != null && next.free) {
+                        size += next.size;
+                        this.next = next.next;
+                        next = next.next;
+                    }
+                }
+            }
+        }
+    }
+}
