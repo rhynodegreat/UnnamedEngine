03628a5db61c1cdf7a881edd08044ae6ef1ec083
rhynodegreat <rhynodegreat@gmail.com>
2017-01-21 19:08:33 -0600

    Add Pool

---
+++ UnnamedEngine/Utilities/Pool.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UnnamedEngine.Utilities {
+    public class Pool<T> {
+        Stack<T> stack;
+        Func<T> create;
+        object locker;
+        int size;
+
+        public int Count {
+            get {
+                return stack.Count;
+            }
+        }
+
+        public Pool(Func<T> creationCallback) {
+            if (creationCallback == null) throw new ArgumentNullException(nameof(creationCallback));
+            create = creationCallback;
+
+            stack = new Stack<T>();
+            locker = new object();
+            size = 4;
+
+            for (int i = 0; i < size; i++) {
+                stack.Push(create());
+            }
+        }
+
+        public T Get() {
+            if (stack.Count == 0) {
+                for (int i = 0; i < size; i++) {
+                    stack.Push(create());
+                    size *= 2;
+                }
+            }
+            return stack.Pop();
+        }
+
+        public void Free(T item) {
+            stack.Push(item);
+        }
+    }
+}
