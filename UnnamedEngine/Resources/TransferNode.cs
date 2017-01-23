5c41f32b08529313fbe3ed5c503d2f2e2236a181
rhynodegreat <rhynodegreat@gmail.com>
2017-01-23 14:00:20 -0600

    Add TransferNode

---
+++ UnnamedEngine/Resources/TransferNode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+using CSGL.Vulkan;
+using Buffer = CSGL.Vulkan.Buffer;
+
+using UnnamedEngine.Core;
+
+namespace UnnamedEngine.Resources {
+    public abstract class TransferNode : CommandNode {
+        protected Device device;
+
+        protected TransferNode(Device device, VkPipelineStageFlags flags) : base(device, flags) {
+
+        }
+
+        public abstract void Transfer<T>(T[] data, Buffer buffer, VkBufferUsageFlags usage) where T : struct;
+    }
+}
