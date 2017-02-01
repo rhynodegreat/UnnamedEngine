31c02d8324799341c795fdc80cd42ac90f86a43a
rhynodegreat <rhynodegreat@gmail.com>
2017-01-31 22:05:31 -0600

    Add ISystem

---
+++ UnnamedEngine/Core/ISystem.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UnnamedEngine.Core {
+    public interface ISystem {
+        void Update(float delta);
+        int Priority { get; set; }
+    }
+}
