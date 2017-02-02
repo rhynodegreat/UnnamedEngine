fcbe0ae81201b4550fa5acbdd7eed0d21d457a48
rhynodegreat <rhynodegreat@gmail.com>
2017-02-02 15:57:12 -0600

    Add LightingNode and OpaqueNode

---
+++ Test/LightingNode.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+using CSGL.Vulkan;
+using UnnamedEngine.Rendering;
+
+namespace Test {
+    public class LightingNode : RenderNode {
+        RenderPass renderPass;
+        uint subpassIndex;
+        CommandPool pool;
+        
+        Pipeline pipeline;
+        List<CommandBuffer> submitBuffers;
+
+        public LightingNode(CommandPool pool) {
+            this.pool = pool;
+        }
+
+        public void Init(Framebuffer framebuffer) {
+            CommandBuffer commandBuffer = pool.Allocate(VkCommandBufferLevel.Secondary);
+
+            CommandBufferInheritanceInfo inheritance = new CommandBufferInheritanceInfo();
+            inheritance.renderPass = renderPass;
+            inheritance.subpass = subpassIndex;
+            inheritance.framebuffer = framebuffer;
+
+            CommandBufferBeginInfo beginInfo = new CommandBufferBeginInfo();
+            beginInfo.flags = VkCommandBufferUsageFlags.RenderPassContinueBit | VkCommandBufferUsageFlags.SimultaneousUseBit;
+            beginInfo.inheritanceInfo = inheritance;
+
+            commandBuffer.Begin(beginInfo);
+
+            commandBuffer.End();
+
+            submitBuffers = new List<CommandBuffer> { commandBuffer };
+        }
+
+        protected override void Bake(RenderPass renderPass, uint subpassIndex) {
+            this.renderPass = renderPass;
+            this.subpassIndex = subpassIndex;
+        }
+
+        public override List<CommandBuffer> GetCommands() {
+            return submitBuffers;
+        }
+    }
+}
---
+++ Test/OpaqueNode.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+using CSGL.Vulkan;
+
+using UnnamedEngine.Rendering;
+
+namespace Test {
+    public class OpaqueNode : RenderNode {
+        RenderPass renderPass;
+        uint subpassIndex;
+
+        List<CommandBuffer> submitBuffers;
+
+        public OpaqueNode(CommandPool pool) {
+            submitBuffers = new List<CommandBuffer>();
+        }
+
+        protected override void Bake(RenderPass renderPass, uint subpassIndex) {
+            this.renderPass = renderPass;
+            this.subpassIndex = subpassIndex;
+        }
+
+        public override List<CommandBuffer> GetCommands() {
+            return submitBuffers;
+        }
+    }
+}
