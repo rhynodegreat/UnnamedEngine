a1c1e047a5de52c6c937b22472dfb35ab1f76ef1
rhynodegreat <rhynodegreat@gmail.com>
2017-02-01 22:55:58 -0600

    Add RenderGraph and RenderNode

---
+++ UnnamedEngine/Rendering/RenderGraph.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+
+using CSGL.Vulkan;
+
+using UnnamedEngine.Core;
+
+namespace UnnamedEngine.Rendering {
+    public class RenderGraph : IDisposable {
+        bool disposed;
+        Engine engine;
+
+        List<AttachmentDescription> attachments;
+        Dictionary<RenderNode, SubpassDescription> nodeMap;
+        List<RenderNode> nodes;
+        List<SubpassDescription> subpasses;
+        List<SubpassDependency> dependencies;
+
+        public IList<AttachmentDescription> Attachments { get; private set; }
+        public RenderPass RenderPass { get; private set; }
+
+        public RenderGraph(Engine engine) {
+            if (engine == null) throw new ArgumentNullException(nameof(engine));
+
+            this.engine = engine;
+
+            attachments = new List<AttachmentDescription>();
+            subpasses = new List<SubpassDescription>();
+            dependencies = new List<SubpassDependency>();
+            Attachments = attachments.AsReadOnly();
+
+            nodeMap = new Dictionary<RenderNode, SubpassDescription>();
+            nodes = new List<RenderNode>();
+        }
+
+        public void Bake() {
+            subpasses.Clear();
+            dependencies.Clear();
+
+            foreach (var node in nodeMap.Keys) {
+                nodes.Add(node);
+                subpasses.Add(nodeMap[node]);
+            }
+
+            foreach (var node in nodeMap.Keys) {
+                Bake(node);
+            }
+
+            RenderPassCreateInfo info = new RenderPassCreateInfo();
+            info.attachments = attachments;
+            info.subpasses = subpasses;
+            info.dependencies = dependencies;
+
+            RenderPass?.Dispose();
+            RenderPass = new RenderPass(engine.Graphics.Device, info);
+        }
+
+        void Bake(RenderNode node) {
+            var desc = nodeMap[node];
+
+            desc.inputAttachments = new List<AttachmentReference>();
+            foreach (var input in node.input) {
+                if (!attachments.Contains(input.attachment)) throw new RenderGraphException("Attachment that is not part of this RenderGraph referenced");
+                desc.inputAttachments.Add(new AttachmentReference { attachment = (uint)attachments.IndexOf(input.attachment), layout = input.layout });
+            }
+
+            desc.colorAttachments = new List<AttachmentReference>();
+            foreach (var color in node.color) {
+                if (!attachments.Contains(color.attachment)) throw new RenderGraphException("Attachment that is not part of this RenderGraph referenced");
+                desc.colorAttachments.Add(new AttachmentReference { attachment = (uint)attachments.IndexOf(color.attachment), layout = color.layout });
+            }
+
+            desc.resolveAttachments = new List<AttachmentReference>();
+            foreach (var resolve in node.resolve) {
+                if (!attachments.Contains(resolve.attachment)) throw new RenderGraphException("Attachment that is not part of this RenderGraph referenced");
+                desc.resolveAttachments.Add(new AttachmentReference { attachment = (uint)attachments.IndexOf(resolve.attachment), layout = resolve.layout });
+            }
+
+            desc.preserveAttachments = new List<uint>();
+            foreach (var preserve in node.preserve) {
+                if (!attachments.Contains(preserve)) throw new RenderGraphException("Attachment that is not part of this RenderGraph referenced");
+                desc.preserveAttachments.Add((uint)attachments.IndexOf(preserve));
+            }
+
+            if (!attachments.Contains(node.DepthStencil)) throw new RenderGraphException("Attachment that is not part of this RenderGraph referenced");
+            desc.depthStencilAttachment = new AttachmentReference { attachment = (uint)attachments.IndexOf(node.DepthStencil), layout = node.DepthStencilLayout };
+
+            foreach (var dependency in node.dependencies) {
+                var subpassDependency = dependency.dependency;
+
+                if (dependency.source != null) {
+                    subpassDependency.srcSubpass = (uint)subpasses.IndexOf(nodeMap[dependency.source]);
+                } else {
+                    subpassDependency.srcSubpass = uint.MaxValue;
+                }
+
+                if (dependency.dest != null) {
+                    subpassDependency.dstSubpass = (uint)subpasses.IndexOf(nodeMap[node]);
+                } else {
+                    subpassDependency.dstSubpass = uint.MaxValue;
+                }
+
+                dependencies.Add(subpassDependency);
+            }
+        }
+
+        public void AddAttachment(AttachmentDescription attachment) {
+            if (attachment == null) throw new ArgumentNullException(nameof(attachment));
+            if (attachments.Contains(attachment)) return;
+
+            attachments.Add(attachment);
+        }
+
+        public void RemoveAttachment(AttachmentDescription attachment) {
+            attachments.Remove(attachment);
+        }
+
+        public void AddNode(RenderNode node) {
+            if (node == null) throw new ArgumentNullException(nameof(node));
+            
+            nodeMap.Add(node, new SubpassDescription());
+        }
+
+        public void RemoveNode(RenderNode node) {
+            nodeMap.Remove(node);
+        }
+
+        public void Render(RenderPassBeginInfo beginInfo, CommandBuffer commandBuffer) {
+            beginInfo.renderPass = RenderPass;
+
+            commandBuffer.BeginRenderPass(beginInfo, VkSubpassContents.SecondaryCommandBuffers);
+
+            for (int i = 0; i < nodes.Count; i++) {
+                commandBuffer.Execute(nodes[i].GetCommands());
+                if (i < nodes.Count - 1) commandBuffer.NextSubpass(VkSubpassContents.SecondaryCommandBuffers);
+            }
+
+            commandBuffer.EndRenderPass();
+        }
+
+        public void Dispose() {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        void Dispose(bool disposing) {
+            if (disposed) return;
+
+            RenderPass?.Dispose();
+
+            disposed = true;
+        }
+
+        ~RenderGraph() {
+            Dispose(false);
+        }
+    }
+
+    public class RenderGraphException : Exception {
+        public RenderGraphException(string message) : base(message) { }
+    }
+}
---
+++ UnnamedEngine/Rendering/RenderNode.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+
+using CSGL.Vulkan;
+
+namespace UnnamedEngine.Rendering {
+    public abstract class RenderNode {
+        internal List<AttachmentPair> input;
+        internal List<AttachmentPair> color;
+        internal List<AttachmentPair> resolve;
+        internal List<AttachmentDescription> preserve;
+        internal List<Dependency> dependencies;
+
+        public AttachmentDescription DepthStencil { get; set; }
+        public VkImageLayout DepthStencilLayout { get; set; }
+
+        public RenderNode() {
+            input = new List<AttachmentPair>();
+            color = new List<AttachmentPair>();
+            resolve = new List<AttachmentPair>();
+            preserve = new List<AttachmentDescription>();
+
+            dependencies = new List<Dependency>();
+        }
+
+        public void AddInput(AttachmentDescription attachment, VkImageLayout layout) {
+            if (attachment == null) throw new ArgumentNullException(nameof(attachment));
+
+            for (int i = 0; i < input.Count; i++) {
+                var pair = input[i];
+                if (pair.attachment == attachment) {
+                    pair.layout = layout;
+                    input[i] = pair;
+                    return;
+                }
+            }
+
+            input.Add(new AttachmentPair { attachment = attachment, layout = layout });
+        }
+
+        public void RemoveInput(AttachmentDescription attachment) {
+            for (int i = 0; i < input.Count; i++) {
+                if (input[i].attachment == attachment) {
+                    input.RemoveAt(i);
+                    return;
+                }
+            }
+        }
+
+        public void AddColor(AttachmentDescription attachment, VkImageLayout layout) {
+            if (attachment == null) throw new ArgumentNullException(nameof(attachment));
+
+            for (int i = 0; i < color.Count; i++) {
+                var pair = color[i];
+                if (pair.attachment == attachment) {
+                    pair.layout = layout;
+                    color[i] = pair;
+                    return;
+                }
+            }
+
+            color.Add(new AttachmentPair { attachment = attachment, layout = layout });
+        }
+
+        public void RemoveColor(AttachmentDescription attachment) {
+            for (int i = 0; i < color.Count; i++) {
+                if (color[i].attachment == attachment) {
+                    color.RemoveAt(i);
+                    return;
+                }
+            }
+        }
+
+        public void AddResolve(AttachmentDescription attachment, VkImageLayout layout) {
+            if (attachment == null) throw new ArgumentNullException(nameof(attachment));
+
+            for (int i = 0; i < resolve.Count; i++) {
+                var pair = resolve[i];
+                if (pair.attachment == attachment) {
+                    pair.layout = layout;
+                    resolve[i] = pair;
+                    return;
+                }
+            }
+
+            resolve.Add(new AttachmentPair { attachment = attachment, layout = layout });
+        }
+
+        public void RemoveResolve(AttachmentDescription attachment) {
+            for (int i = 0; i < resolve.Count; i++) {
+                if (resolve[i].attachment == attachment) {
+                    resolve.RemoveAt(i);
+                    return;
+                }
+            }
+        }
+
+        public void AddPreserve(AttachmentDescription attachment) {
+            if (attachment == null) throw new ArgumentNullException(nameof(attachment));
+            if (preserve.Contains(attachment)) return;
+
+            preserve.Add(attachment);
+        }
+
+        public void RemovePreserce(AttachmentDescription attachment) {
+            preserve.Remove(attachment);
+        }
+
+        public void AddSource(RenderNode source, SubpassDependency dependency) {
+            if (dependency == null) throw new ArgumentNullException(nameof(dependency));
+
+            for (int i = 0; i < dependencies.Count; i++) {
+                var dep = dependencies[i];
+                if (dep.dependency == dependency) {
+                    dep.source = source;
+                    dependencies[i] = dep;
+                    return;
+                }
+            }
+
+            dependencies.Add(new Dependency { source = source, dest = this, dependency = dependency });
+        }
+
+        public void AddDest(RenderNode dest, SubpassDependency dependency) {
+            if (dependency == null) throw new ArgumentNullException(nameof(dependency));
+
+            for (int i = 0; i < dependencies.Count; i++) {
+                var dep = dependencies[i];
+                if (dep.dependency == dependency) {
+                    dep.dest = dest;
+                    dependencies[i] = dep;
+                    return;
+                }
+            }
+
+            dependencies.Add(new Dependency { source = this, dest = dest, dependency = dependency });
+        }
+
+        public void RemoveDependency(SubpassDependency dependency) {
+            for (int i = 0; i < dependencies.Count; i++) {
+                if (dependencies[i].dependency == dependency) {
+                    dependencies.RemoveAt(i);
+                    return;
+                }
+            }
+        }
+
+        public abstract List<CommandBuffer> GetCommands();
+
+        internal struct AttachmentPair {
+            public AttachmentDescription attachment;
+            public VkImageLayout layout;
+        }
+
+        internal struct Dependency {
+            public RenderNode source;
+            public RenderNode dest;
+            public SubpassDependency dependency;
+        }
+    }
+}
