c074bfc579736488659fcb063f3ec892114cb559
rhynodegreat <rhynodegreat@gmail.com>
2017-02-09 21:08:54 -0600

    Add CommandGraph and CommandNode

---
+++ UnnamedEngine/Core/CommandGraph.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using CSGL.Vulkan;
+
+namespace UnnamedEngine.Core {
+    public class CommandGraph : IDisposable {
+        bool disposed;
+
+        HashSet<CommandNode> nodes;
+        List<CommandNode> nodeList;
+        List<CommandBuffer> commands;
+
+        public CommandGraph() {
+            nodes = new HashSet<CommandNode>();
+            nodeList = new List<CommandNode>();
+            commands = new List<CommandBuffer>();
+        }
+
+        public void AddNode(CommandNode node) {
+            nodes.Add(node);
+        }
+
+        public void RemoveNode(CommandNode node) {
+            nodes.Remove(node);
+        }
+
+        struct SortState {
+            public bool open;
+            public bool finished;
+        }
+
+        public void Bake() {
+            //https://en.wikipedia.org/wiki/Topological_sorting#Depth-first_search
+            Stack<CommandNode> stack = new Stack<CommandNode>();
+            Dictionary<CommandNode, SortState> sortState = new Dictionary<CommandNode, SortState>();
+
+            foreach (var node in nodes) {
+                sortState.Add(node, new SortState());
+            }
+
+            foreach (var node in nodes) {
+                Visit(stack, sortState, node);
+            }
+
+            nodeList.Clear();
+            commands.Clear();
+
+            while (stack.Count > 0) {
+                var node = stack.Pop();
+
+                nodeList.Add(node);
+                node.Bake();
+                commands.Add(null);
+            }
+        }
+
+        void Visit(Stack<CommandNode> stack, Dictionary<CommandNode, SortState> sortState, CommandNode node) {
+            if (sortState[node].open) throw new CommandGraphException("Nodes do not form Direct Acyclic Graph");
+            if (!sortState[node].finished) {
+                var state = sortState[node];
+                state.open = true;
+                sortState[node] = state;
+
+                foreach (var output in node.Output) {
+                    Visit(stack, sortState, output);
+                }
+
+                state.open = false;
+                state.finished = true;
+                sortState[node] = state;
+
+                stack.Push(node);
+            }
+        }
+
+        public List<CommandBuffer> GetCommands() {
+            Parallel.For(0, nodeList.Count, GetCommands);
+            return commands;
+        }
+
+        void GetCommands(int i) {
+            commands[i] = null;
+            try {
+                commands[i] = nodeList[i].GetCommands();
+            }
+            catch (Exception e) {
+                Console.WriteLine(e);
+            }
+        }
+
+        public void Dispose() {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        void Dispose(bool disposing) {
+            if (disposed) return;
+
+            if (disposing) {
+                foreach (var node in nodes) {
+                    node.Dispose();
+                }
+            }
+
+            disposed = true;
+        }
+
+        ~CommandGraph() {
+            Dispose(false);
+        }
+    }
+}
---
+++ UnnamedEngine/Core/CommandNode.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+
+using CSGL.Vulkan;
+
+namespace UnnamedEngine.Core {
+    public abstract class CommandNode : IDisposable {
+        bool disposed;
+        List<CommandNode> input;
+        List<CommandNode> output;
+        List<Event> waitEvents;
+
+        public IList<CommandNode> Input { get; private set; }
+        public IList<CommandNode> Output { get; private set; }
+
+        public List<MemoryBarrier> MemoryBarriers { get; set; }
+        public List<BufferMemoryBarrier> BufferMemoryBarriers { get; set; }
+        public List<ImageMemoryBarrier> ImageMemoryBarriers { get; set; }
+
+        public VkPipelineStageFlags SrcStage { get; set; }
+        public VkPipelineStageFlags DstStage { get; set; }
+
+        public Event Event { get; private set; }
+        public VkPipelineStageFlags EventStage { get; set; }
+
+        protected CommandNode(Device device) {
+            Event = new Event(device);
+            input = new List<CommandNode>();
+            output = new List<CommandNode>();
+            Input = input.AsReadOnly();
+            Output = output.AsReadOnly();
+            waitEvents = new List<Event>();
+        }
+
+        public abstract CommandBuffer GetCommands();
+
+        public void WaitEvents(CommandBuffer commandBuffer) {
+            commandBuffer.WaitEvents(waitEvents, SrcStage, DstStage, MemoryBarriers, BufferMemoryBarriers, ImageMemoryBarriers);
+        }
+
+        public void SetEvents(CommandBuffer commandBuffer) {
+            commandBuffer.ResetEvent(Event, EventStage);
+            commandBuffer.SetEvent(Event, EventStage);
+        }
+
+        public void AddInput(CommandNode node) {
+            if (node == null) throw new ArgumentNullException(nameof(node));
+            if (input.Contains(node)) return;
+            input.Add(node);
+            node.output.Add(this);
+        }
+
+        public void RemoveInput(CommandNode node) {
+            if (input.Remove(node)) {
+                node.output.Remove(this);
+            }
+        }
+
+        internal void Bake() {
+            waitEvents.Clear();
+            foreach (var node in input) {
+                waitEvents.Add(node.Event);
+            }
+        }
+
+        public void Dispose() {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing) {
+            if (disposed) return;
+
+            Event.Dispose();
+
+            disposed = true;
+        }
+
+        ~CommandNode() {
+            Dispose(false);
+        }
+    }
+}
