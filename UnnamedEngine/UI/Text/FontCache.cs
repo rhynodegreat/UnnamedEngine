b9c0a348305ca5d2625c2d22f1da8a3f42eae4fc
rhynodegreat <rhynodegreat@gmail.com>
2017-02-12 17:30:39 -0600

    Add FontCache and FontCachePage

---
+++ UnnamedEngine/UI/Text/FontCache.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+using CSGL.Graphics;
+using CSGL.Vulkan;
+
+using UnnamedEngine.Core;
+
+namespace UnnamedEngine.UI.Text {
+    public class FontCache : IDisposable {
+        bool disposed;
+
+        Engine engine;
+
+        public FontCache(Engine engine) {
+            if (engine == null) throw new ArgumentNullException(nameof(engine));
+
+            this.engine = engine;
+        }
+
+        public void Dispose() {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        void Dispose(bool disposing) {
+            if (disposed) return;
+
+            disposed = true;
+        }
+
+        ~FontCache() {
+            Dispose(false);
+        }
+    }
+}
---
+++ UnnamedEngine/UI/Text/FontCachePage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+using CSGL.Graphics;
+using CSGL.Math;
+
+namespace UnnamedEngine.UI.Text {
+    internal class FontCachePage {
+        bool[] free;
+        int width;
+        int height;
+
+        public Bitmap<Color3> Bitmap { get; private set; }
+
+        public FontCachePage(int width, int height) {
+            this.width = width;
+            this.height = height;
+
+            Bitmap = new Bitmap<Color3>(width, height);
+            free = new bool[width * height];
+
+            for (int i = 0; i < width * height; i++) {
+                free[i] = true;
+            }
+        }
+
+        int GetIndex(int x, int y) {
+            return x + y * width;
+        }
+
+        public bool CanAdd(Rectangle rect) {
+            return true;
+        }
+    }
+}
