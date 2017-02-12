5b8debf804708cb0f2a5060da4d95fc549c441a4
rhynodegreat <rhynodegreat@gmail.com>
2017-02-12 16:36:48 -0600

    Add UIElement and Label

---
+++ UnnamedEngine/UI/Label.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UnnamedEngine.UI {
+    public class Label : UIElement {
+        public string Text { get; set; }
+    }
+}
---
+++ UnnamedEngine/UI/UIElement.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Numerics;
+using System.Collections.Generic;
+
+using UnnamedEngine.Rendering;
+
+namespace UnnamedEngine.UI {
+    public abstract class UIElement {
+        public Vector2 Position { get; set; }
+    }
+}
