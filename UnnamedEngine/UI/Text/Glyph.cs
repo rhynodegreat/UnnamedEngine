64db371338d8b9d731bd54acfa5b2177d5e14293
rhynodegreat <rhynodegreat@gmail.com>
2017-02-13 15:42:23 -0600

    Add Glyph

---
+++ UnnamedEngine/UI/Text/Glyph.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+using MSDFGen;
+
+namespace UnnamedEngine.UI.Text {
+    public class Glyph {
+        public Font Font { get; private set; }
+        public Shape Shape { get; private set; }
+        public int Codepoint { get; private set; }
+
+        internal Glyph(Font font, Shape shape, int codepoint) {
+            Font = font;
+            Shape = shape;
+            Codepoint = codepoint;
+        }
+    }
+}
