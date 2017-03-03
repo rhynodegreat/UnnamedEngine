3441aaffcfd8f366c4f9d3c867361ef28b669944
rhynodegreat <rhynodegreat@gmail.com>
2017-03-03 14:47:58 -0600

    Add Far plane capability to Camera
    Camera can use either finite Far plane distance or set it to infinity

--- UnnamedEngine/Rendering/Camera.cs
+++ UnnamedEngine/Rendering/Camera.cs
@@ -28,6 +28,8 @@ namespace UnnamedEngine.Rendering {
         public Transform Transform { get; private set; }
         public float FOV { get; set; }
         public float Near { get; set; }
+        public float Far { get; set; }
+        public bool Infinite { get; set; }
 
         public DescriptorSetLayout Layout {
             get {
@@ -41,7 +43,7 @@ namespace UnnamedEngine.Rendering {
             }
         }
 
-        public Camera(Engine engine, Window window, float fov, float near) {
+        public Camera(Engine engine, Window window, float fov, float near, float far, bool infinite) {
             if (engine == null) throw new ArgumentNullException(nameof(engine));
             if (window == null) throw new ArgumentNullException(nameof(window));
 
@@ -51,6 +53,8 @@ namespace UnnamedEngine.Rendering {
 
             FOV = fov;
             Near = near;
+            Far = far;
+            Infinite = infinite;
 
             Transform = new Transform();
 
@@ -60,8 +64,16 @@ namespace UnnamedEngine.Rendering {
             Update();
         }
 
+        public Camera(Engine engine, Window window, float fov, float near) : this(engine, window, fov, near, 0, true) { }
+        public Camera(Engine engine, Window window, float fov, float near, float far) : this(engine, window, fov, near, far, false) { }
+
         internal void Update() {
-            projection = CreatePerspective(FOV, window.Width / (float)window.Height, Near);
+            if (Infinite) {
+                projection = CreatePerspectiveInfinite(FOV, window.Width / (float)window.Height, Near);
+            } else {
+                projection = CreatePerspective(FOV, window.Width / (float)window.Height, Near, Far);
+            }
+
             projection.M22 *= -1;
             view = Matrix4x4.CreateLookAt(Transform.Position, Transform.Position + Transform.Forward, Transform.Up);
 
@@ -70,7 +82,7 @@ namespace UnnamedEngine.Rendering {
             alloc.memory.Unmap();
         }
 
-        Matrix4x4 CreatePerspective(float fov, float aspect, float near) {
+        Matrix4x4 CreatePerspectiveInfinite(float fov, float aspect, float near) {
             //creates projection matrix with reversed Z and infinite far plane
             //https://nlguillemot.wordpress.com/2016/12/07/reversed-z-in-opengl/
             //http://dev.theomader.com/depth-precision/
@@ -83,6 +95,22 @@ namespace UnnamedEngine.Rendering {
                 0, f, 0, 0,
                 0, 0, 0, -1f,
                 0, 0, near, 0);
+        }
+
+        Matrix4x4 CreatePerspective(float fov, float aspect, float near, float far) {
+            //creates projection matrix with reversed Z and infinite far plane
+            //http://dev.theomader.com/depth-precision/
+
+            float fovRad = fov * (float)(Math.PI / 180);
+            float f = 1f / (float)Math.Tan(fovRad / 2);
+
+            float a = -(Far / (Near - Far));
+
+            return new Matrix4x4(
+                f / aspect, 0, 0, 0,
+                0, f, 0, 0,
+                0, 0, a - 1, -1f,
+                0, 0, near * a, 0);
         }
 
         void CreateDescriptor() {
