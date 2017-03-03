a840c427b832abfc6ac68703cdce2b7bd34c0965
rhynodegreat <rhynodegreat@gmail.com>
2017-03-02 21:54:18 -0600

    Revert "Refactor Group"
    This reverts commit 7db71e07bd6548bda1d0386d71427702f04415ea.

--- UnnamedEngine/ECS/Group.cs
+++ UnnamedEngine/ECS/Group.cs
@@ -1,23 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Reflection;
 
 namespace UnnamedEngine.ECS {
     public class Group : IDisposable {
         bool disposed;
 
-        struct Pair {
-            public Action<Entity, object> add;
-            public Action<Entity, object> remove;
-        }
-
         EntityManager manager;
         HashSet<Type> typesSet;
-        Dictionary<Type, object> componentMap;
+        Dictionary<Type, IList<object>> componentMap;
+        Dictionary<Type, List<object>> realComponentMap;
         Dictionary<object, Entity> reverseComponentMap;
         HashSet<Entity> entitySet;
         List<Entity> realEntities;
-        Dictionary<Type, Pair> delegateMap;
 
         public IList<Entity> Entities;
         public IList<Type> Types { get; private set; }
@@ -29,21 +23,20 @@ namespace UnnamedEngine.ECS {
             this.manager = manager;
 
             typesSet = new HashSet<Type>();
-            componentMap = new Dictionary<Type, object>();
+            realComponentMap = new Dictionary<Type, List<object>>();
+            componentMap = new Dictionary<Type, IList<object>>();
             reverseComponentMap = new Dictionary<object, Entity>();
             entitySet = new HashSet<Entity>();
             realEntities = new List<Entity>();
-            delegateMap = new Dictionary<Type, Pair>();
 
             Entities = realEntities.AsReadOnly();
             Types = new List<Type>(types).AsReadOnly();
 
             for (int i = 0; i < types.Length; i++) {
                 typesSet.Add(types[i]);
-                Type listType = typeof(List<>).MakeGenericType(types[i]);
-                object list = Activator.CreateInstance(listType);
-                componentMap.Add(types[i], list);
-                CreateDelegates(types[i]);
+                List<object> list = new List<object>();
+                realComponentMap.Add(types[i], list);
+                componentMap.Add(types[i], list.AsReadOnly());
             }
 
             manager.OnComponentAdded += OnComponentAdded;
@@ -53,7 +46,9 @@ namespace UnnamedEngine.ECS {
         void OnComponentAdded(Entity entity, object component) {
             if (typesSet.Contains(component.GetType())) {
                 Type t = component.GetType();
-                delegateMap[t].add(entity, component);
+                List<object> list = realComponentMap[t];
+                list.Add(component);
+                reverseComponentMap.Add(component, entity);
 
                 entitySet.Add(entity);
                 realEntities.Add(entity);
@@ -63,7 +58,9 @@ namespace UnnamedEngine.ECS {
         void OnComponentRemoved(Entity entity, object component) {
             if (typesSet.Contains(component.GetType()) && entitySet.Contains(entity)) {
                 Type t = component.GetType();
-                delegateMap[t].remove(entity, component);
+                List<object> list = realComponentMap[t];
+                list.Remove(component);
+                reverseComponentMap.Remove(component);
                 
                 //make sure entities with multiple of the same type of component are not removed
                 for (int i = 0; i < entity.Components.Count; i++) {
@@ -75,33 +72,12 @@ namespace UnnamedEngine.ECS {
             }
         }
 
-        void CreateDelegates(Type t) {
-            MethodInfo add = typeof(Group).GetMethod(nameof(AddComponent), BindingFlags.NonPublic);
-            add.MakeGenericMethod(t);
-            Action<Entity, object> addDelegate = (Action<Entity, object>)Delegate.CreateDelegate(typeof(Action<Entity, object>), add);
-
-            MethodInfo remove = typeof(Group).GetMethod(nameof(RemoveComponent), BindingFlags.NonPublic);
-            remove.MakeGenericMethod(t);
-            Action<Entity, object> removeDelegate = (Action<Entity, object>)Delegate.CreateDelegate(typeof(Action<Entity, object>), remove);
-
-            delegateMap.Add(t, new Pair { add = addDelegate, remove = removeDelegate });
+        public IList<object> GetComponents(Type type) {
+            return componentMap[type];
         }
 
-        void AddComponent<T>(Entity entity, object component) {
-            List<T> list = (List<T>)componentMap[typeof(T)];
-            list.Add((T)component);
-            reverseComponentMap.Add(component, entity);
-        }
-
-        void RemoveComponent<T>(Entity entity, object component) {
-            List<T> list = (List<T>)componentMap[typeof(T)];
-            list.Remove((T)component);
-            reverseComponentMap.Remove(componentMap);
-        }
-
-        public List<T> GetComponents<T>() {
-            if (!typesSet.Contains(typeof(T))) throw new GroupException("Type is not part of this group");
-            return (List<T>)componentMap[typeof(T)];
+        public IList<object> GetComponent<T>() {
+            return componentMap[typeof(T)];
         }
 
         public void Dispose() {
@@ -120,9 +96,5 @@ namespace UnnamedEngine.ECS {
         ~Group() {
             Dispose(false);
         }
-    }
-
-    public class GroupException : Exception {
-        public GroupException(string message) : base(message) { }
     }
 }
