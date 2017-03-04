01396fcf38bfd2fdad550f2a3db292fdea68ae10
rhynodegreat <rhynodegreat@gmail.com>
2017-03-03 21:26:37 -0600

    Add UIRenderer

---
+++ UnnamedEngine/UI/UIRenderer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+using CSGL.Vulkan;
+
+namespace UnnamedEngine.UI {
+    public abstract class UIRenderer : IDisposable {
+        public abstract void Render(CommandBuffer commandbuffer);
+
+        public void Dispose() {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing) { }
+
+        ~UIRenderer() {
+            Dispose(false);
+        }
+    }
+}
