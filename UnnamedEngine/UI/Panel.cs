c5df60613eed17ec5205894f23276354edeca0a2
rhynodegreat <rhynodegreat@gmail.com>
2017-03-03 22:02:23 -0600

    Add Panel and PanelRenderer

---
+++ UnnamedEngine/UI/Panel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+using CSGL.Graphics;
+
+namespace UnnamedEngine.UI {
+    public class Panel : UIElement {
+        Color4 Color { get; set; }
+    }
+}
---
+++ UnnamedEngine/UI/PanelRenderer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+using CSGL.Vulkan;
+
+using UnnamedEngine.Core;
+
+namespace UnnamedEngine.UI {
+    public class PanelRenderer : IRenderer {
+        bool disposed;
+
+        public void Render(CommandBuffer commandBuffer) {
+
+        }
+
+        public void Dispose() {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        void Dispose(bool disposing) {
+            if (disposed) return;
+
+            disposed = true;
+        }
+
+        ~PanelRenderer() {
+            Dispose(false);
+        }
+    }
+}
