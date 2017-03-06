c509b02db2f01d0f568ba65e3a78ea847806c9c8
rhynodegreat <rhynodegreat@gmail.com>
2017-03-06 15:57:28 -0600

    Add TextRenderer
    Add Harfbuzz

---
+++ UnnamedEngine/UI/TextRenderer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+using CSGL.Vulkan;
+
+using UnnamedEngine.ECS;
+using UnnamedEngine.Rendering;
+
+namespace UnnamedEngine.UI {
+    public class TextRenderer : UIRenderer {
+        bool disposed;
+
+        public void PreRender(Entity e, Transform transform, UIElement element) {
+
+        }
+
+        public void Render(CommandBuffer commandbuffer, Entity e, Transform transform, UIElement element) {
+
+        }
+
+        public void Dispose() {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        void Dispose(bool disposing) {
+            if (disposed) return;
+
+            disposing = true;
+        }
+
+        ~TextRenderer() {
+
+        }
+    }
+}
